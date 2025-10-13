061049b56d1d7176b8c66f31e793d6c1d394fd75
Robert <barrosor@oregonstate.edu>
2025-10-13 11:56:09 -0400

    chore: Initialized main.py where main program will live later on in the project

---
+++ main.py
@@ -0,0 +1,7 @@
+# Robert Barroso
+# CS361-400-F2025 Software Engineering I
+# Assignment 2 - Environment Setup, Course Project Plan, and Spring 1 Plan
+# Repo: https://github.com/robertbarroso/CS361-Milestone1
+
+
+print("Hello World")
